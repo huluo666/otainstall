aac40c6a3ea46e26ff707daa86ad395ab6b7981d
jenkins <huluo@leniu.com>
2022-03-25 09:28:24 +0800

    添加调试信息

--- hello.py
+++ hello.py
@@ -1,3 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Date    : 2022/3/17
+# @Author  : jenkins
+# @Version : V1.0
+# @Features:
+
 from flask import Flask
 from flask import render_template, make_response, send_file
 from flask import request, redirect, url_for,session
